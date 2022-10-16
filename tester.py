31703999c385e6ade750cb50deb1ebd1097cac9e
caldwella2 <caldwella2@allegheny.edu>
2022-10-15 22:46:59 -0400

    added payload list and my other test code

---
+++ tester.py
@@ -0,0 +1,28 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.select import Select
+import time
+import os
+
+def sendScripts():
+    # This function works
+    driver.find_element(By.ID, 'query').send_keys("<Script>alert('Script Passed.')</scripT>")
+    # this finds the button
+    button = driver.find_element(By.ID,'button')
+    time.sleep(10) # sleeps for 5 seconds
+    # this clicks the button
+    driver.execute_script("arguments[0].click();", button)
+    time.sleep(5) # sleeps for 5 seconds
+    driver.quit() # closes the window
+
+if __name__ == "__main__":
+    clear = lambda: os.system('cls')
+    clear() # clear screen
+    print("[+] Welcome to SeQure!\n") # starting prompt
+    url = input("[+] Enter the URL you would like to check: ")
+    driver = webdriver.Chrome(executable_path="C:/Users/alexi/Desktop/chromedriver/chromedriver") # path to chromedriver
+    driver.get(url) # opens an automated browser
+    sendScripts()
+    # https://xss-game.appspot.com/level1/frame
+
+
