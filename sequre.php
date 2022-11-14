0d500b10700f6ef317590c2bf5d861f88cae6d2a
unknown <caldwella2@allegheny.edu>
2022-11-14 10:45:50 -0500

    added sequre php file

---
+++ sequre.php
@@ -0,0 +1,9 @@
+<?php
+/**
+* Plugin Name: SeQure
+* Plugin URI: https://github.com/ReadyResearchers/SeQure
+* Description: Test.
+* Version: 0.1
+* Author: Alexis Caldwell
+* Author URI: https://lex.chompe.rs/
+**/
